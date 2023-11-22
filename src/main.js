e6223b1846adff342a6359203200f90c9bd39e68
taektu_coding <taektu.coding@gmail.com>
2023-11-22 10:11:06 +0100

    feat: tailwindcss setup

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
-import App from './App.vue'
+import "@/index.css";
+import App from './App.vue';
 
 const app = createApp(App)
 
